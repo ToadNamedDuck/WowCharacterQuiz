038f302531872b07e7524a8269da1a70c28acb7c
Jason Harris <lord.jtharris@gmail.com>
2023-11-02 14:29:33 -0500

    Added a couple more questions.

--- Components/Quiz.js
+++ Components/Quiz.js
@@ -5,8 +5,10 @@ import QuizQuestion from "./QuizQuestion";
 
 export default function Quiz(){
     const q1 = new Question("Do you have a preference for Alliance or Horde?", ["For the Alliance!", "For the Horde!", "I don't care who pays me."]);
-    const q2 = new Question("Do you prefer ranged or melee today?", ["Yes, definitely", "I'm not a coward", "I don't care"])
-    const questionArray = [q1, q2];
+    const q2 = new Question("Do you prefer ranged or melee today?", ["Yes, definitely", "I'm not a coward", "I just want to see something die."]);
+    const q3 = new Question("Do you want to think and press a lot of buttons while you play?", ["Complex classes are for me", "Hell no", "Next question please."]);
+    const q4 = new Question("Is your aesthetic shiny and bright, or more muted?", ["Definitely shiny. I am the best.", "I like it dark and depressing.", "Still don't care."]);
+    const questionArray = [q1, q2, q3, q4];
     return <>
     <div>
         <h1>Let's ask about your playstyle preferences!</h1>
