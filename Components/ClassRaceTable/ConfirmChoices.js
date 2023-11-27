c61e08f935c787033c595fcdf9dfbe78ac0688be
Jason Harris <lord.jtharris@gmail.com>
2023-11-26 20:43:09 -0600

    Logic in place for selecting a class and a race dynamically. Button tells you what to play in console log atm

--- Components/ClassRaceTable/ConfirmChoices.js
+++ Components/ClassRaceTable/ConfirmChoices.js
@@ -1,12 +1,22 @@
-export default function ConfirmChoices({classGrayedOutStates, raceGrayedOutStates}){
-    function buttonOnClick(e){
+export default function ConfirmChoices({ classGrayedOutStates, raceGrayedOutStates }) {
+    function buttonOnClick(e) {
         e.preventDefault();
         let validRaces = raceGrayedOutStates.filter(race => race.grayedOutState !== true);
         let validClasses = classGrayedOutStates.filter(classObj => classObj.grayedOutState !== true);
-        console.log(validRaces)
-        console.log(validClasses)
+        let selectedClass;
+        let narrowedRaces;
+        let ValidOptionChosen = false;
+        while (ValidOptionChosen === false) {
+            selectedClass = validClasses[Math.floor(Math.random() * validClasses.length)]
+            narrowedRaces = validRaces.filter(race => race.validClasses.includes(selectedClass));
+            if (narrowedRaces.length !== 0) {
+                ValidOptionChosen = true;
+            }
+        }
+        const selectedRace = narrowedRaces[Math.floor(Math.random() * narrowedRaces.length)]
+        console.log(`You should play a ${selectedRace.name} ${selectedClass.name}!`)
     }
     return <>
-        <button onClick={(e) => buttonOnClick(e)}>Click Me</button>
+        <button onClick={(e) => buttonOnClick(e)}>Tell Me What To Play!!!</button>
     </>
 }
