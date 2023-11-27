ec584300ce23ebcdbfc428e874b4eade2d7e192a
Jason Harris <lord.jtharris@gmail.com>
2023-11-26 19:55:08 -0600

    Realizing the last giant chunk of work last week was useless is a fun experience. Button does console log of valid race class.

--- Components/ClassRaceTable/ConfirmChoices.js
+++ Components/ClassRaceTable/ConfirmChoices.js
@@ -1,3 +1,12 @@
 export default function ConfirmChoices({classGrayedOutStates, raceGrayedOutStates}){
-
+    function buttonOnClick(e){
+        e.preventDefault();
+        let validRaces = raceGrayedOutStates.filter(race => race.grayedOutState !== true);
+        let validClasses = classGrayedOutStates.filter(classObj => classObj.grayedOutState !== true);
+        console.log(validRaces)
+        console.log(validClasses)
+    }
+    return <>
+        <button onClick={(e) => buttonOnClick(e)}>Click Me</button>
+    </>
 }
