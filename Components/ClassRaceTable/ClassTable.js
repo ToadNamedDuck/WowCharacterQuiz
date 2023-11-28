ea1b0451b4042e6931100c837616de3257bcd877
Jason Harris <lord.jtharris@gmail.com>
2023-11-28 13:33:35 -0600

    Some CSS styling

--- Components/ClassRaceTable/ClassTable.js
+++ Components/ClassRaceTable/ClassTable.js
@@ -2,12 +2,10 @@ import IndividualCR from "./IndividualCR"
 
 export default function ClassTable({arrayOfClassObjects, questionStateArray}){
     return <div id="classTable">
-        <div id="HordeRaces">
             {
                 arrayOfClassObjects.map(pClass => {
                     return <IndividualCR key={`Class-${pClass.name}`} questionStateArray={questionStateArray} ClassOrRaceObject={pClass}/>
                 })
             }
-        </div>
     </div>
 }
--- Components/ClassRaceTable/RaceTable.js
+++ Components/ClassRaceTable/RaceTable.js
@@ -1,29 +1,31 @@
 import { Faction } from "@/app/page"
 import IndividualCR from "./IndividualCR"
 
-export default function RaceTable({arrayOfRaceObjects, questionStateArray}){
+export default function RaceTable({ arrayOfRaceObjects, questionStateArray }) {
     return <div id="raceTable">
-        <div id="AllianceRaces">
-        {
-                arrayOfRaceObjects.map(race => {
-                    if(race.faction === Faction.Alliance)
-                    return <IndividualCR key={`Alliance-${race.name}`} questionStateArray={questionStateArray} ClassOrRaceObject={race}/>
-                })
-            }
-        </div>
-        <div id="HordeRaces">
-            {
-                arrayOfRaceObjects.map(race => {
-                    if(race.faction === Faction.Horde)
-                    return <IndividualCR key={`Horde-${race.name}`} questionStateArray={questionStateArray} ClassOrRaceObject={race}/>
-                })
-            }
+        <div id="non-neutral-Races">
+            <div id="AllianceRaces">
+                {
+                    arrayOfRaceObjects.map(race => {
+                        if (race.faction === Faction.Alliance)
+                            return <IndividualCR key={`Alliance-${race.name}`} questionStateArray={questionStateArray} ClassOrRaceObject={race} />
+                    })
+                }
+            </div>
+            <div id="HordeRaces">
+                {
+                    arrayOfRaceObjects.map(race => {
+                        if (race.faction === Faction.Horde)
+                            return <IndividualCR key={`Horde-${race.name}`} questionStateArray={questionStateArray} ClassOrRaceObject={race} />
+                    })
+                }
+            </div>
         </div>
         <div id="NeutralRaces">
             {
                 arrayOfRaceObjects.map(race => {
-                    if(race.faction === Faction.Both)
-                    return <IndividualCR key={`A-H-${race.name}`} questionStateArray={questionStateArray} ClassOrRaceObject={race}/>
+                    if (race.faction === Faction.Both)
+                        return <IndividualCR key={`A-H-${race.name}`} questionStateArray={questionStateArray} ClassOrRaceObject={race} />
                 })
             }
         </div>
